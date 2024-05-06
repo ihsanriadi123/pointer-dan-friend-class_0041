ebc7912e36f6324b95b46a202fb2fb54d92048bb
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-05-06 14:59:56 +0700

    class mahasiswa

---
+++ pointer.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+    public:
+    int nim;
+    void shownim() {
+        cout <<"No Induk = " << nim << endl;
+    }
+};
+
+
+int main() {
+    mahasiswa mhs(1);
+    mhs.shownim();
+
+    mahasiswa &refmhs = mhs;
+    refmhs.nim = 2;
+    mhs.shownim();
+
+    mahasiswa *pmhs = &mhs;
+    pmhs ->nim = 3;
+    pmhs ->shownim();
+    return 0;
+}
