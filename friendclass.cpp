1efa1d5b85c8fc5293decc7afd6e00fa04d4f145
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2024-05-06 15:23:21 +0700

    membuat tampilan nama nya

--- friendclass.cpp
+++ friendclass.cpp
@@ -11,4 +11,32 @@ class orang {
         friend class siswa;
 };
 
+class siswa {
+    private:
+        int id;
+    public:
+        void setid(int pid);
+        void displayall(orang &a);
+};
 
+void siswa::displayall(orang&a) {
+    cout << id ,, endl << a.nama;
+}
+
+void orang::setnama(string pnama) {
+    nama = pnama;
+}
+
+void siswa::setid(int pid) {
+    id = pid;
+}
+
+
+int main () {
+    orang joko;
+    joko.setnama("joko susilo");
+    siswa joko_siswa;
+    joko_siswa.setid(1);
+    joko_siswa.displayall(joko);
+    return 0;
+}
